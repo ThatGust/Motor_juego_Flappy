2c3c66a15c149e17392e53c14449191f574caf1e
Jenny Ha <jhuancaan@unsa.edu.pe>
2021-07-31 15:36:55 -0500

    Reorganización de archivos multimedia

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Juego creado para el curso de Ciencia de la Computación II
 - Para ejecutar en linux ejecutar los siguientes comandos:
   - `g++ -c *.cpp`
   - `g++ *.o -o app -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system -lsfml-network`
+  - `./app`
+
+- Los archivos multimedia (imágenes, sonido, etc ) del juego se encuentran en la carpeta *res*.
--- SFML Template/DEFINITIONS.h
+++ SFML Template/DEFINITIONS.h
@@ -5,36 +5,37 @@
 
 #define SPLASH_STATE_SHOW_TIME 2.0
 
-#define SPLASH_SCENE_BACKGROUND_FILEPATH "Splash Background.png"
-#define MAIN_MENU_BACKGROUND_FILEPATH "sky.png"
-#define GAME_BACKGROUND_FILEPATH "sky.png"
-#define GAME_OVER_BACKGROUND_FILEPATH "sky.png"
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "res/img/Splash Background.png"
+#define MAIN_MENU_BACKGROUND_FILEPATH "res/img/sky.png"
+#define GAME_BACKGROUND_FILEPATH "res/img/sky.png"
+#define GAME_OVER_BACKGROUND_FILEPATH "res/img/sky.png"
 
-#define GAME_TITLE_FILEPATH "title.png"
-#define PLAY_BUTTON_FILEPATH "PlayButton.png"
+#define GAME_TITLE_FILEPATH "res/img/title.png"
+#define PLAY_BUTTON_FILEPATH "res/img/PlayButton.png"
 
-#define PIPE_UP_FILEPATH "PipeUp.png"
-#define PIPE_DOWN_FILEPATH "PipeDown.png"
+#define PIPE_UP_FILEPATH "res/img/PipeUp.png"
+#define PIPE_DOWN_FILEPATH "res/img/PipeDown.png"
    
-#define LAND_FILEPATH "Land.png"
+#define LAND_FILEPATH "res/img/Land.png"
+// #define LAND_FILEPATH "res/img/land_of_tutorial.png"
 
-#define VIRUS_FRAME_1_FILEPATH "virus_01.png"
-#define VIRUS_FRAME_2_FILEPATH "virus_02.png"
-#define VIRUS_FRAME_3_FILEPATH "virus_03.png"
-#define VIRUS_FRAME_4_FILEPATH "virus_04.png"
+#define VIRUS_FRAME_1_FILEPATH "res/img/virus_01.png"
+#define VIRUS_FRAME_2_FILEPATH "res/img/virus_02.png"
+#define VIRUS_FRAME_3_FILEPATH "res/img/virus_03.png"
+#define VIRUS_FRAME_4_FILEPATH "res/img/virus_04.png"
 
 #define PIPE_MOVEMENT_SPEED 200.0f
-#define PIPE_SPAWN_FRECUENCY 2.5f
+#define PIPE_SPAWN_FRECUENCY 2.5f       // A menor valor, pipes aparecen más juntos
 
-#define VIRUS_ANIMATION_DURATION 0.7f  // Variar para lograr una mejor animation
+#define VIRUS_ANIMATION_DURATION 0.7f  // Variar para lograr una mejor animation del virus
 
 #define VIRUS_STATE_STILL 1
 #define VIRUS_STATE_FALLING 2
 #define VIRUS_STATE_FLAYING 3
 
 #define GRAVITY 350.0f
-#define FLYING_SPEED 350.0f
 
+#define FLYING_SPEED 350.0f
 #define FLYING_DURATION 0.25f
 
 #define ROTATION_SPEED 100.0f
